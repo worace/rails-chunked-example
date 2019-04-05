781835d74dd8f34dcd1c25a77ee13823c88a079b
Horace Williams <horace.d.williams@gmail.com>
2019-04-04 17:25:03 -0700

    Show fixed example

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,18 @@
 class ApplicationController < ActionController::Base
   def index
-    response.set_header("Transfer-Encoding", "chunked")
+    # Uncomment this line to break things:
+    # response.set_header("Transfer-Encoding", "chunked")
     response.set_header("X-Accel-Buffering", "no")
     response.set_header("Cache-Control", "no-cache")
     response.set_header("Last-Modified", Time.zone.now.ctime.to_s)
     response.delete_header("Content-Length")
 
-    puts "before"
     self.response_body = Enumerator.new do |y|
-      5.times do |i|
-        puts "yield row #{i}"
-        y << i.to_s
+      100.times do |i|
+        y << i.to_s + "\n"
+        sleep(0.01)
       end
     end
-    puts "after"
 
     response.status = 200
   end
